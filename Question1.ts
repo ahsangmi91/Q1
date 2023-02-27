bf98e91ac71c392506d4d04663e55255e4916e80
Muhammad Ahsan <125314437+ahsangmi91@users.noreply.github.com>
2023-02-28 02:27:39 +0500

    Add files via upload

---
+++ Question1.ts
@@ -0,0 +1,12 @@
+/*Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
+//Answer is mentioned below.
+
+
+
+
+
+
+
+var name1 = "Muhammad Ahsan"
+console.log("hello " + name1 + " would you like to learn typescript?")
